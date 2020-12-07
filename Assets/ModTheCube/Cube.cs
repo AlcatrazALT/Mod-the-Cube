c2773977f23c43958f756e6a7291ceaf2d18a5c5
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-12-07 22:13:43 +0700

    Change color with coroutine

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -1,12 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
     private MeshRenderer material;
+    private Color color;
     private Vector3 newPosition = new Vector3(1, 1, 1);
     private readonly float newSize = 2;
     private float rotationSpeed;
     private float timeSinceChange = 0f;
+
+    [SerializeField]
+    private bool canChangeColor = true;
 
     [SerializeField]
     private float timeToChange = 1f;
@@ -19,11 +24,17 @@ public class Cube : MonoBehaviour
         transform.localScale = Vector3.one * newSize;
 
         material = GetComponent<MeshRenderer>();
+        StartCoroutine("ChangeRandomColorWithCoroutine");
     }
 
     private void Update()
     {
         transform.Rotate(Vector3.forward, Time.deltaTime * rotationSpeed);
+        //ChangeRandomColor();
+    }
+
+    private void ChangeRandomColor()
+    {
         timeSinceChange += Time.deltaTime;
         if (material != null && timeSinceChange >= timeToChange)
         {
@@ -31,5 +42,28 @@ public class Cube : MonoBehaviour
             material.material.color = newColor;
             timeSinceChange = 0;
         }
+    }
+
+    private IEnumerator ChangeRandomColorWithCoroutine()
+    {
+        color = material.material.color;
+
+        float r = Random.Range(0, 1.0f);
+        float g = Random.Range(0, 1.0f);
+        float b = Random.Range(0, 1.0f);
+
+        Color newColor = new Color(r, g, b);
+
+        float t = 0;
+
+        while (t < 1)
+        {
+            material.material.color = Color.Lerp(color, newColor, t);
+            t += Time.deltaTime / timeToChange;
+            yield return null;
+        }
+        yield return new WaitForSeconds(timeToChange);
+
+        StartCoroutine("ChangeRandomColorWithCoroutine");
     }
 }
