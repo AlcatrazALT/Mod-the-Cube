9590930469a5ce48ab5ffd9db4fe36cb8928b187
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-12-07 20:30:10 +0700

    Change the cube behavior

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -1,23 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
-    void Start()
+
+    private Vector3 newPosition = new Vector3(1, 1, 1);
+    private readonly float newSize = 2;
+
+    [SerializeField]
+    private float rotationSpeed = 15.0f;
+
+    private void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
+        transform.position = newPosition;
+        transform.localScale = Vector3.one * newSize;
+
         Material material = Renderer.material;
-        
+
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
     }
-    
-    void Update()
+
+    private void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(0.0f, rotationSpeed * Time.deltaTime, 0.0f);
     }
 }
